b23be22a0f0a40d8e78fd3bb7cdea5e983c021a8
Reva1463 <revaoctavina88@gmail.com>
2024-09-22 12:41:36 +0700

    Add files via upload

---
+++ praktik Aritmatikaa.py
@@ -0,0 +1,12 @@
+#input user
+a = int(input("masukan bilangan  pertama: ")) #user menginputkan bilangan pertama
+b = int(input("masukan bilangan kedua: ")) #user menginputkan bilangan kedua
+
+#hasil operasi penjumlahan
+print ("hasil penjumlahan:" , a+b) 
+#hasil operasi pengurangan
+print ("hasil pengurangan:" , a-b)
+#hasil operasi perkalian
+print ("hasil perkalian:", a*b)
+#hasil operasai pembagian
+print ("hasil pembagian:", a/b)
---
+++ praktik luas lingkaran.py
@@ -0,0 +1,13 @@
+#input user
+jari_jari = float(input("masukan jari_jari: ")) #user memasukkan panjang jari_jari lingkaran
+
+phi = float("3.14") #menetapkan bilangan phi
+
+#variabel rumus
+luas = jari_jari * jari_jari * phi #rumus luas lignkaran
+keliling = 2 * phi * jari_jari  #rumus keliling lingkaran
+
+#hasil luas lingkaran
+print("luas lingkaran: ", luas)
+#hasil keliling lingkaran
+print ("keliling lingkaran: ", keliling)
