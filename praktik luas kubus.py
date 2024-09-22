821e4dbd01d68ac677e0b2046f8fdaa54614efd4
Reva1463 <revaoctavina88@gmail.com>
2024-09-22 12:37:18 +0700

    Add files via upload

---
+++ praktik luas kubus.py
@@ -0,0 +1,11 @@
+#imput user
+sisi=float((input("masukan sisi kubus: "))) #user menginputkan panjang sisi kubus
+
+#varibel rumus
+luas = 6 * sisi * sisi #rumus luas kubus
+volume = sisi * sisi * sisi #rumus voume kubus
+
+#3hasil luas kubus 
+print("Luas Kubus: ", luas)
+#hasil volume kubus
+print("Volume Kubus: ", volume)
